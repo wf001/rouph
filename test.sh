b9c96deeafbd39e5b407676797eab72bcfa94632
wf001 <wf001@diax.xyz>
2021-10-29 20:21:54 +0900

    Add unary(testing skiped).

--- test.sh
+++ test.sh
@@ -34,6 +34,11 @@ assert 41 " 12 + 34 - 5 "
 assert 6 "2*3 "
 assert 7 "2*3+1"
 assert 8 "(3-1)*4"
+assert 4 "8/2"
+assert 4 "(3+5)/2"
+assert 8 " 24 - 20 + (6- 4)*2 "
+# Cant recieve '-' from cmd arg, but it works.
+# assert -3 "-3"
 
 rm -f tmp.s
 
