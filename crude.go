89a54d35f398bf210f51ddf2c880f6fea109817b
wf001 <tarkiew@gmail.com>
2021-10-30 19:44:20 +0900

    Add semicolons separating multiple statements.

--- crude.go
+++ crude.go
@@ -3,10 +3,13 @@ package main
 func main() {
 	head := TokenizeHandler()
 	printToken(head)
-	_, node := Expr(head.Next)
-
-	Info("%s\n", "=================")
-	printNode(node)
+	_, node := Program(head.Next)
+    n := node
+	Info("%s\n", "======== print node =========")
+	for ; n != nil; n = n.Next {
+        printNode(n)
+        Info("%s\n", "## [ref next]")
+    }
 	Info("%s\n", "=================")
 	codegen(node)
 
