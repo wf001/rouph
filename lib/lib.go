6e596c60cd6563301869bf9e1dbf98cc1fdad28f
wf001 <tarkiew@gmail.com>
2021-11-23 21:01:18 +0900

    Support standard out of string.

--- lib/lib.go
+++ lib/lib.go
@@ -7,28 +7,26 @@ import (
 func LibPrint() {
 	fmt.Println("")
 }
-func StdlibHandler(libName string, target string, length int) {
-	if libName == "puts" {
-		libPuts(target, length)
-	} else {
-		panic("not found std lib")
-	}
+func StdlibHandler() {
+	libPuts()
 }
 
-func libPuts(name string, length int) {
+func libPuts() {
 	fmt.Printf(".puts:\n")
-	fmt.Printf("  push offset %s\n", name)
-	fmt.Printf("  mov rsi, [rsp]\n")
-	fmt.Printf("  mov rdx, %d\n", length)
+	fmt.Printf("  mov rdx, 1\n")
 	fmt.Printf("  mov rax, 1\n")
 	fmt.Printf("  mov rdi, 1\n")
 	fmt.Printf("  syscall\n")
-	fmt.Printf("  pop rax\n")
+	fmt.Printf("  sub r8, 1\n")
+	fmt.Printf("  add rsi, 1\n")
+	fmt.Printf("  cmp r8, 0\n")
+	fmt.Printf("  jnz .puts\n")
 
 	fmt.Printf("  push 0xa\n")
 	fmt.Printf("  mov rsi, rsp\n")
 	fmt.Printf("  mov rax, 1\n")
 	fmt.Printf("  syscall\n")
 	fmt.Printf("  pop rax\n")
+    fmt.Printf("  mov rax, 1\n")
 	fmt.Printf("  ret\n")
 }
