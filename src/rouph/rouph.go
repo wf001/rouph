c91565166f37c1a40063a2443afdd2884c677b40
wf001 <wf001@diax.xyz>
2021-11-28 21:00:08 +0900

    Support rouph build/run/help command.

--- src/rouph/rouph.go
+++ src/rouph/rouph.go
@@ -4,23 +4,55 @@ import (
 	"flag"
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
 func main() {
 	flag.Parse()
-	file := flag.Arg(0)
+	cmd := flag.Arg(0)
+	file := flag.Arg(1)
+	fs := strings.Split(file, "/")
+	exec_file := strings.Split(fs[len(fs)-1], ".")[0]
+	if cmd == "build" {
+		rouphBuild(file, exec_file)
+	} else if cmd == "run" {
+		rouphBuild(file, exec_file)
+		rouphRun(exec_file)
+	} else {
+		rouphHelp()
+	}
+
+}
+
+func rouphBuild(file string, exec_file string) {
 	out, err := exec.Command("sh", "-c", "./bin/rouphc "+file+" > test.s").CombinedOutput()
 	if err != nil {
-		fmt.Printf("CombineOutput: %s, Error: %v\n", out, err)
+		fmt.Printf("1rouph: %s, Error: %v\n", out, err)
 	}
 
-	out, err = exec.Command("gcc", "-static", "-o", "test", "test.s").CombinedOutput()
+	out, err = exec.Command("gcc", "-static", "-o", exec_file, "test.s").CombinedOutput()
 	if err != nil {
-		fmt.Printf("CombineOutput: %s, Error: %v\n", out, err)
+		fmt.Printf("rouph: Error: %v", err)
 	}
-
 	_, err = exec.Command("rm", "-f", "test.s").CombinedOutput()
 	if err != nil {
-		fmt.Printf("CombineOutput: %s, Error: %v\n", out, err)
+		fmt.Printf("rouph: %s, Error: %v\n", out, err)
 	}
+
+}
+func rouphRun(exec_file string) {
+	out, err := exec.Command("./" + exec_file).CombinedOutput()
+	fmt.Printf("%s\n", out)
+	if err != nil {
+		fmt.Printf("rouph: Error: %v\n", err)
+	}
+	_, err = exec.Command("rm", "-f", exec_file).CombinedOutput()
+	if err != nil {
+		fmt.Printf("rouph: %s, Error: %v\n", out, err)
+	}
+
+}
+
+func rouphHelp() {
+	fmt.Printf("this is help.\n")
 }
